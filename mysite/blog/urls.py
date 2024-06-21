d3d4b626d28a0a9056c67f625c34f8a27cd54bc1
k-lila <krishnalilapalandi@gmail.com>
2024-06-21 17:15:09 -0300

    Black Formatter

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.RootView.as_view(), name='root'),
-    path('home/', views.PostView.as_view(), name='home')
+    path("", views.RootView.as_view(), name="root"),
+    path("home/", views.PostView.as_view(), name="home"),
 ]
--- mysite/tests/models/test_post.py
+++ mysite/tests/models/test_post.py
@@ -2,11 +2,12 @@ import pytest
 
 from blog.factories import PostFactory
 
-@pytest.fixture
 
+@pytest.fixture
 def post_published():
-    return PostFactory(title='pytest with factory')
+    return PostFactory(title="pytest with factory")
+
 
 @pytest.mark.django_db
 def test_create_published_post(post_published):
-    assert post_published.title == 'pytest with factory'
+    assert post_published.title == "pytest with factory"
